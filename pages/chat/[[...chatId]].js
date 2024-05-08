5ed8718d41923e743a51601adc8611a21fdd87a0
Oussafadi <fadi.oussa@outlook.fr>
2024-05-08 15:08:11 +0100

    auth0 startup code

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Chat() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Chat Page</h1>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
+'use client';
 import Head from "next/head";
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from "next/link";
 
 export default function Home() {
+  const {user,error,isLoading} = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+
   return (
     <div>
       <Head>
         <title>Next JS ChatGPT Starter</title>
       </Head>
       <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+      { user && 
+            <Link href="/api/auth/logout">
+            Logout
+            </Link>
+      }
+      { !user && 
+            <Link href="/api/auth/login">
+            Login</Link>
+      }
     </div>
   );
 }
